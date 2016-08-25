0e0bf547dbea7b2f304f8b4b7eabe9748a2d4b6e
luobinbin <binbin520898@163.com>
2016-08-25 13:35:48 +0800

    test

--- webpack.config.js
+++ webpack.config.js
@@ -3,51 +3,56 @@ var webpack = require('webpack');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var openBrowserWebpackPlugin = require('open-browser-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('bundle','bundle.js');
+var commonsPlugin = new webpack.optimize.CommonsChunkPlugin({
+    name:'bundle',
+    filename:'[name].[hash].js'
+});
 
 module.exports = {
-    entry:{
-        index: [
-            'webpack/hot/dev-server',
-            'webpack-dev-server/client?http://localhost:8080',
-            path.resolve(__dirname, 'app/js/index.js')],
+    entry    : {
+        index : [path.resolve(__dirname, 'app/js/index.js')],
         bundle: [
-                    path.resolve(__dirname, 'mc_lib/angular/angular.js'),
-                    path.resolve(__dirname, 'mc_lib/angular-route/angular-route.js')
-                ]
+            path.resolve(__dirname, 'mc_lib/angular/angular.js'),
+            path.resolve(__dirname, 'mc_lib/angular-route/angular-route.js')
+        ]
     },
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'index.js'
+    output   : {
+        path      : path.resolve(__dirname, 'build'),
+        filename  : '[name].[hash].js'
     },
-    resolve: {
+    resolve  : {
         extension: ['', '.js', '.json', '.less']
     },
-    module: {
+    module   : {
         loaders: [
             {
-                test: /\.js$/,
+                test   : /\.js$/,
                 loaders: [],
                 exclude: path.resolve(__dirname, 'node_modules')
             },
             {
-                test: /\.css/,
+                test  : /\.css/,
                 loader: ExtractTextPlugin.extract("style-loader", "css-loader")
             },
             {
-                test: /\.less/,
+                test  : /\.less/,
                 loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
             }
         ]
     },
     devServer: {
-        hot: true
+        stats      : {colors: true},
+        contentBase: '/build',
+        hot        : true,
+        inline     : true,
+        progress   : true,
+        port       : 8080
     },
-    plugins: [
+    plugins  : [
         commonsPlugin,
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoErrorsPlugin(),
-        new openBrowserWebpackPlugin({ url: 'http://localhost:8080' }),
+        new openBrowserWebpackPlugin({url: 'http://localhost:8080'}),
         new ExtractTextPlugin("styles.css"),
         new HtmlWebpackPlugin({
             filename: 'index.html',
